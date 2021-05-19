aaee40faecf5bf00b8534835955774711e936ca2
震 方 <a1521442784@live.com>
2021-05-19 12:57:37 -0700

    [Tower] -Added Tower follow (now is to the player) -Added Tower basic animation

--- Tyrant/Assets/Scripts/Tower.cs
+++ Tyrant/Assets/Scripts/Tower.cs
@@ -5,14 +5,35 @@ using UnityEngine;
 public class Tower : MonoBehaviour
 {
     // Start is called before the first frame update
+    public Animator animator;
+    public Transform targetpos;
+    private Vector2 direction;
     void Start()
     {
-        
+        animator = GetComponent<Animator>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        direction = (transform.position - targetpos.position).normalized;
+        if (direction.x >0.8f)
+        {
+            animator.SetFloat("Direction", 0.1f);
+        }
+        if (direction.y < -0.8f)
+        {
+            animator.SetFloat("Direction", 0.333f);
+        }
+        if (direction.x < -0.8f)
+        {
+            animator.SetFloat("Direction", 0.66f);
+        }
+        if (direction.y > 0.8f)
+        {
+            animator.SetFloat("Direction", 1.0f);
+        }
+
+
     }
 }

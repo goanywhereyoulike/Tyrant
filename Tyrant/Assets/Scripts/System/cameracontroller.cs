21a27ff6e6ce59e3d9760c1113de1055d7b1df15
william chen <53885588+YourFather69420@users.noreply.github.com>
2021-05-26 15:10:55 -0700

    zhiming chen
    - added camera size
    - now able to adjust camera size in runtime

--- Tyrant/Assets/Scripts/System/cameracontroller.cs
+++ Tyrant/Assets/Scripts/System/cameracontroller.cs
@@ -5,15 +5,16 @@ using UnityEngine;
 public class cameracontroller : MonoBehaviour
 {
     public Transform target;
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    public float playerViewZone = 0.0f;
+    public float playerCamera { get { return playerViewZone; } set { playerViewZone = value; }  }
 
-    // Update is called once per frame
+    private void Awake()
+    {
+        playerViewZone = Camera.main.orthographicSize;
+    }
     void Update()
     {
+        Camera.main.orthographicSize = playerViewZone;
         transform.position = new Vector3(target.transform.position.x, target.transform.position.y,transform.position.z);
     }
 }

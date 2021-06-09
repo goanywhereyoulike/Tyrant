49db9991cc068148b8b74fded26b5a35577fb3f7
Fang Zhen <36040048+goanywhereyoulike@users.noreply.github.com>
2021-06-08 20:26:13 -0700

    Zhen fang (#12)
    * [PreProduction]
    -Added tower and tilemap to prefab
    
    * [Project file]
    -Place TowerTexture to Textures folder
    
    * [Tower]
    -reorganize the project
    -added base
    
    * [Tower]
    -Added base and recover range
    -Change tower target to enemy
    
    * fix some bugs
    
    Co-authored-by: 震 方 <a1521442784@live.com>

--- Tyrant/Assets/Scripts/Tower/TowerBullet.cs
+++ Tyrant/Assets/Scripts/Tower/TowerBullet.cs
@@ -3,17 +3,30 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class TowerBullet : MonoBehaviour
-{      
+{
+    [SerializeField]
+    float bulletDamage = 5.0f;
     public GameObject hitEffect;
     private void OnTriggerEnter2D(Collider2D collision)
     {
         string tag = collision.gameObject.tag;
+        Enemy enemy = collision.gameObject.GetComponent<Enemy>();
         if (tag == "Player" || tag == "Wall" || tag == "Tower")
         {
             GameObject effect = Instantiate(hitEffect, transform.position, Quaternion.identity);
             Destroy(effect, 0.3f);
+
             gameObject.SetActive(false);
             //Destroy(gameObject);
+        }
+
+        if (enemy)
+        {
+            enemy.TakeDamage(bulletDamage);
+            GameObject effect = Instantiate(hitEffect, transform.position, Quaternion.identity);
+            Destroy(effect, 0.3f);
+            gameObject.SetActive(false);
+
         }
 
     }
--- Tyrant/Assets/Scripts/Tower/TowerShoot.cs
+++ Tyrant/Assets/Scripts/Tower/TowerShoot.cs
@@ -4,6 +4,8 @@ using UnityEngine;
 
 public class TowerShoot : MonoBehaviour
 {
+    private GameObject currentTarget = null;
+    Tower tower;
     PlayerMovement player;
     public Transform ShootPoint;
     private Vector2 ShootOffset;
@@ -12,34 +14,45 @@ public class TowerShoot : MonoBehaviour
     public float DistanceToShoot;
     public float BulletForce = 20.0f;
     [SerializeField]
-    float FireRate = 0.1f;
+    float FireRate;
     float WaitFire = 0.0f;
+
+    public Animator animator;
+    private Vector3 direction;
+    float angle;
     void Start()
     {
         player = FindObjectOfType<PlayerMovement>();
+        tower = GetComponent<Tower>();
         TargetPos = player.transform;
     }
 
     // Update is called once per frame
     void Update()
     {
-        WaitFire += Time.deltaTime;
+        UpdateTarget();
 
-        float Distance = (TargetPos.position - transform.position).sqrMagnitude;
-        if (Distance < DistanceToShoot * DistanceToShoot)
+        if (currentTarget)
         {
-            if (WaitFire > FireRate) //Fires gun everytime timer exceeds firerate
+            WaitFire += Time.deltaTime;
+
+            float Distance = (currentTarget.transform.position - transform.position).sqrMagnitude;
+            if (Distance < DistanceToShoot * DistanceToShoot)
             {
-                WaitFire = 0.0f;
-                Fire();
+                if (WaitFire > FireRate) //Fires gun everytime timer exceeds firerate
+                {
+                    WaitFire = 0.0f;
+                    Fire();
+                }
             }
         }
+
 
     }
 
     void Fire()
     {
-        Vector3 Direction = (TargetPos.position - ShootPoint.position).normalized;
+        Vector3 Direction = (currentTarget.transform.position - ShootPoint.position).normalized;
         GameObject bullet = ObjectPoolManager.Instance.GetPooledObject("TowerBullet");
         if (bullet)
         {
@@ -49,6 +62,100 @@ public class TowerShoot : MonoBehaviour
             Rigidbody2D rb = bullet.GetComponent<Rigidbody2D>();
 
             rb.AddForce(Direction * BulletForce, ForceMode2D.Impulse);
+        }
+
+
+    }
+
+    void UpdateTarget()
+    {
+        Enemy[] enemies = FindObjectsOfType<Enemy>();
+        GameObject target = GetClosestTarget(enemies);
+
+        if (target != null)
+        {
+            float Distance = (TargetPos.position - transform.position).sqrMagnitude;
+            if (Distance < DistanceToShoot * DistanceToShoot)
+            {
+                currentTarget = target;
+                TowerToTarget(target.transform);
+            }
+        }
+        else
+        {
+            currentTarget = null;
+        }
+    }
+
+    GameObject GetClosestTarget(Enemy[] enemies)
+    {
+        GameObject retEnemy = null;
+        for (int i = 0; i < enemies.Length; ++i)
+        {
+            if (!enemies[i].IsDead)
+            {
+                if (retEnemy == null)
+                {
+                    retEnemy = enemies[i].gameObject;
+                }
+                else if ((enemies[i].transform.position - transform.position).sqrMagnitude < (retEnemy.transform.position - transform.position).sqrMagnitude)
+                {
+                    retEnemy = enemies[i].gameObject;
+                }
+            }
+        }
+        return retEnemy;
+    }
+
+    public void TowerToTarget(Transform targetpos)
+    {
+
+        direction = (targetpos.position - transform.position).normalized;
+        angle = Mathf.Atan2(direction.y, direction.x) * Mathf.Rad2Deg;
+
+        //Left
+        if ((angle < -160.0f && angle >= -180.0f) || (angle > 160.0f && angle <= 180.0f))
+        {
+            animator.SetFloat("Direction", 0.05f);
+        }
+        //Left Up
+        if (angle <= 160.0f && angle >= 100.0f)
+        {
+            animator.SetFloat("Direction", 0.2f);
+
+        }
+        //Up
+        if (angle < 100.0f && angle > 80.0f)
+        {
+            animator.SetFloat("Direction", 0.3f);
+
+        }
+        //Right Up
+        if (angle <= 80.0f && angle >= 10.0f)
+        {
+            animator.SetFloat("Direction", 0.5f);
+        }
+        //Right
+        if (angle < 10.0f && angle > -10.0f)
+        {
+            animator.SetFloat("Direction", 0.6f);
+        }
+        //Right Down
+        if (angle <= -10.0f && angle >= -80.0f)
+        {
+            animator.SetFloat("Direction", 0.72f);
+        }
+        //Down
+        if (angle < -80.0f && angle > -100.0f)
+        {
+            animator.SetFloat("Direction", 0.9f);
+
+        }
+        //Left Down
+        if (angle <= -100.0f && angle >= -160.0f)
+        {
+            animator.SetFloat("Direction", 1.0f);
+
         }
 
 

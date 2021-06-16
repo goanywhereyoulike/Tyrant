fc6e0db1c6004484bf7259c644369e04b7e98eaf
DESKTOP-H2K2OPM\pochun <doghot0625@gmail.com>
2021-06-16 00:41:18 -0700

    Revert "Revert "Revert "Merge branch 'Develop' into PochunChen"""
    This reverts commit 21e8da4200ff3e8f57b53ba4d13af8654cd238dc.

--- Tyrant/Assets/Scripts/Tower/PlayerMovement.cs
+++ Tyrant/Assets/Scripts/Tower/PlayerMovement.cs
@@ -4,14 +4,6 @@ using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
-    public enum PlayerFacing
-    {
-        Up,
-        Down,
-        Left,
-        Right
-    }
-    public PlayerFacing playerFacing;
     Animator MoveAnimator;
     // Start is called before the first frame update
     private void Awake()
@@ -22,11 +14,10 @@ public class PlayerMovement : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        playerFacing = PlayerFacing.Down;
         Vector2 mPosition = transform.position;
         if (InputManager.Instance.GetKey("GoRight"))
         {
-            playerFacing = PlayerFacing.Right;
+
             MoveAnimator.SetTrigger("MoveRight");
             mPosition.x += 0.01f;
             transform.position = mPosition;
@@ -34,7 +25,6 @@ public class PlayerMovement : MonoBehaviour
         }
         if (InputManager.Instance.GetKey("GoLeft"))
         {
-            playerFacing = PlayerFacing.Left;
             MoveAnimator.SetTrigger("MoveLeft");
             mPosition.x -= 0.01f;
             transform.position = mPosition;
@@ -42,7 +32,6 @@ public class PlayerMovement : MonoBehaviour
         }
         if (InputManager.Instance.GetKey("GoUp"))
         {
-            playerFacing = PlayerFacing.Up;
             MoveAnimator.SetTrigger("MoveUp");
             mPosition.y += 0.01f;
             transform.position = mPosition;
@@ -50,7 +39,6 @@ public class PlayerMovement : MonoBehaviour
         }
         if (InputManager.Instance.GetKey("GoDown"))
         {
-            playerFacing = PlayerFacing.Down;
             MoveAnimator.SetTrigger("MoveDown");
             mPosition.y -= 0.01f;
             transform.position = mPosition;

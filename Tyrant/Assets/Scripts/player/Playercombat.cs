ebe1508a6faa4b3db58cbfb33f08165630521946
william chen <53885588+YourFather69420@users.noreply.github.com>
2021-06-15 20:42:03 -0700

    zhiming chen
    -fixed bullet behaviour
    -added bullet range
    -updated healthpotion texture
    -updated bullet asset

--- Tyrant/Assets/Scripts/player/Playercombat.cs
+++ Tyrant/Assets/Scripts/player/Playercombat.cs
@@ -6,14 +6,21 @@ public class Playercombat : MonoBehaviour
 {
     private Gun gun = null;
     public Transform meleePoint;
+    public Transform firePosition;
+    GameObject Playerbullet;
+    
+    public GameObject BulletPrefab;
     public float meleeRange = 1.0f;
     public float meleeSpeed = 2.0f;
     public Inventory mInventory { get; set; }
     float timeBtwAttack = 0f;
-    public LayerMask enemyLayers;
+    LayerMask enemyLayers;
 
+  
     void Update()
     {
+       
+
         if(Time.time>= timeBtwAttack)
         { 
              if (InputManager.Instance.GetKeyDown("Melee"))
@@ -33,17 +40,36 @@ public class Playercombat : MonoBehaviour
         //{
         //    mInventory.DropPickUp(transform.position, mInventory.GetPickUp("gun"));
         //}
-
+       
         if (InputManager.Instance.GetKeyDown("Fire"))
         {
+            
             if (gun)
             {
                 Vector2 playerHeading;
-                playerHeading = (InputManager.Instance.MouseWorldPosition - gameObject.transform.position).normalized;
-                gun.Fire(playerHeading);
+                Vector2 mousePosition = new Vector2(InputManager.Instance.MouseWorldPosition.x, InputManager.Instance.MouseWorldPosition.y);
+                Vector2 selfPosition = new Vector2(gameObject.transform.position.x, gameObject.transform.position.y);
+                playerHeading = (mousePosition-selfPosition).normalized;
+                Fire(playerHeading);
             }
         }
-       
+
+    }
+
+
+    public void Fire(Vector2 playerHeading)
+    {
+        
+        Playerbullet = ObjectPoolManager.Instance.GetPooledObject("bullet");    
+        if (Playerbullet)
+        {
+            Playerbullet.SetActive(true);
+            Playerbullet.transform.position = firePosition.position;
+            Playerbullet.GetComponent<bullet>().Direction = playerHeading;
+            Playerbullet.GetComponent<bullet>().startPosition = firePosition.position;
+        }
+
+        //Instantiate(bullet, firePosition.position, firePosition.rotation);
     }
     void meleeAttack()
     {

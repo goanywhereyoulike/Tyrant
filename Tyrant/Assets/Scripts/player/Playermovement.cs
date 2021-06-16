6450708bff5bfb85b5edcb9b3872d87474d4a6c4
震 方 <a1521442784@live.com>
2021-06-16 10:18:38 -0700

    [Tower] -fixed preset bug
    [Base]
    -Added destroyed base image

--- Tyrant/Assets/Scripts/player/Playermovement.cs
+++ Tyrant/Assets/Scripts/player/Playermovement.cs
@@ -21,6 +21,6 @@ public class Playermovement : MonoBehaviour
 
     private void FixedUpdate()
     {
-        rb.MovePosition(rb.position + movement * moveSpeed*Time.fixedDeltaTime);
+        rb.MovePosition(rb.position + movement * moveSpeed * Time.fixedDeltaTime);
     }
 }

70b20f8535da042998bd3a4bdd4822edb0f64df1
steven-lilith <66922938+steven-lilith@users.noreply.github.com>
2021-06-15 23:18:31 -0700

    Steven gan (#17)
    * Weapon Rotation
    
    -fix weapon pick up and equip
    -add weapon rotation
    
    * Weapon Rotate
    
    -added weapon rotation
    -adjust fire position
    
    * Revert "Merge branch 'Develop' into StevenGan"
    
    This reverts commit 1c199df303fd17b46c77ad354bbd5cc53ceb349c, reversing
    changes made to 05ff5c2ae26da8e59cf07fc9c5a548866e2a4fc3.
    
    * Revert "Revert "Merge branch 'Develop' into StevenGan""
    
    This reverts commit cad3ae8b7dc33c9b49054e1a87065d5378277bf4.
    
    * Merged pick up and rotate weapon
    
    -pickup weapon and stick to player
    -rotate weapon with mouse aim
    -fire at muzzle position

--- Tyrant/Assets/Scripts/Tower/PlayerMovement.cs
+++ Tyrant/Assets/Scripts/Tower/PlayerMovement.cs
@@ -4,6 +4,14 @@ using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
+    public enum PlayerFacing
+    {
+        Up,
+        Down,
+        Left,
+        Right
+    }
+    public PlayerFacing playerFacing;
     Animator MoveAnimator;
     // Start is called before the first frame update
     private void Awake()
@@ -14,10 +22,11 @@ public class PlayerMovement : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        playerFacing = PlayerFacing.Down;
         Vector2 mPosition = transform.position;
         if (InputManager.Instance.GetKey("GoRight"))
         {
-
+            playerFacing = PlayerFacing.Right;
             MoveAnimator.SetTrigger("MoveRight");
             mPosition.x += 0.01f;
             transform.position = mPosition;
@@ -25,6 +34,7 @@ public class PlayerMovement : MonoBehaviour
         }
         if (InputManager.Instance.GetKey("GoLeft"))
         {
+            playerFacing = PlayerFacing.Left;
             MoveAnimator.SetTrigger("MoveLeft");
             mPosition.x -= 0.01f;
             transform.position = mPosition;
@@ -32,6 +42,7 @@ public class PlayerMovement : MonoBehaviour
         }
         if (InputManager.Instance.GetKey("GoUp"))
         {
+            playerFacing = PlayerFacing.Up;
             MoveAnimator.SetTrigger("MoveUp");
             mPosition.y += 0.01f;
             transform.position = mPosition;
@@ -39,6 +50,7 @@ public class PlayerMovement : MonoBehaviour
         }
         if (InputManager.Instance.GetKey("GoDown"))
         {
+            playerFacing = PlayerFacing.Down;
             MoveAnimator.SetTrigger("MoveDown");
             mPosition.y -= 0.01f;
             transform.position = mPosition;

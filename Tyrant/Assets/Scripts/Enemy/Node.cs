7cd2dafa63680d066b7dd441a490425ae55a38bd
DESKTOP-H2K2OPM\pochun <doghot0625@gmail.com>
2021-07-22 13:49:16 -0700

    PochunChen
    -add spawn Manager
    -add spawn Area
    -add spawn wave

---
+++ Tyrant/Assets/Scripts/Enemy/Node.cs
@@ -0,0 +1,28 @@
+//using System.Collections;
+//using System.Collections.Generic;
+//using UnityEngine;
+
+//public class Node
+//{
+//    private List<Node> neighbors = new List<Node>();
+
+//    private Node parent;
+//    private int c = 0;
+//    private int r = 0;
+
+//    private float g = 0.0f;
+//    private float h = 0.0f;
+//    private bool opened = false;
+//    private bool closed = false;
+
+//    public Node Parent { get => parent; set => parent = value; }
+//    public int C { get => c; set => c = value; }
+//    public int R { get => r; set => r = value; }
+//    public float G { get => g; set => g = value; }
+//    public float H { get => h; set => h = value; }
+//    public bool Opened { get => opened; set => opened = value; }
+//    public bool Closed { get => closed; set => closed = value; }
+//    public List<Node> Neighbors { get => neighbors; set => neighbors = value; }
+
+//    //public Node(List<Node> neighbors, int c,int r,float g,float h,bool opened, bool closed)
+//}

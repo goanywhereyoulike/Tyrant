cd73930775c8b7a4759857c6f8db0414f25e8656
DESKTOP-H2K2OPM\pochun <doghot0625@gmail.com>
2021-07-22 17:36:21 -0700

    pochun
    repush

--- Tyrant/Assets/Scripts/Enemy/EnemyManager.cs
+++ Tyrant/Assets/Scripts/Enemy/EnemyManager.cs
@@ -9,94 +9,27 @@ public class EnemyManager : MonoBehaviour
     public static EnemyManager Instance { get => _instance; }
 
   //  List<Enemy> enemies = new List<Enemy>();
-    public int wavenumber=5;
+
     public int noSpawn;
     public Tilemap tilemap;
-    List<GameObject> enemies = new List<GameObject>();
+   
     Enemy enemy;
-    Vector2 spawnPosition;
-    Vector2 insideMax;
-    Vector2 insideMin;
-    int spawnX;
-    int spawnY;
-    int spawnCount;
-    GameObject enemyObject;
+ 
     // public Transform[] spawnPoints;
     private void Start()
     {
-        ObjectPoolManager.Instance.InstantiateObjects("normalenemy");
         if (_instance == null)
         {
             _instance = this;
         }
         else if(_instance !=this)
         {
-            Destroy(this.enemyObject);
+            Destroy(gameObject);
         }
     }
 
-    void Update()
+    private void Update()
     {
-        insideMax = new Vector2(tilemap.cellBounds.xMax, tilemap.cellBounds.yMax) / noSpawn;
-        insideMin = new Vector2(tilemap.cellBounds.xMin, tilemap.cellBounds.yMin) / noSpawn;
-
-
-        if(noSpawn<2)
-        {
-            Debug.Log("can't spawn");
-            return;
-        }
-
-        for(int i =0;i< enemies.Count;++i)
-        {
-            if(enemies[i].activeInHierarchy ==false)
-            {
-                enemies.RemoveAt(i);
-                spawnCount--;
-            }
-        }
-
-        if (spawnCount == 0)
-        {
-            while (spawnCount != wavenumber)
-            {
-                spawnX = Random.Range(tilemap.cellBounds.xMin, tilemap.cellBounds.xMax);
-                spawnY = Random.Range(tilemap.cellBounds.yMin, tilemap.cellBounds.yMax);
-                spawnPosition = new Vector2(spawnX, spawnY);
-                if (CheckPosition(spawnPosition))
-                {
-                    EnemySpawn(spawnPosition);
-                    print("spawn");
-                }
-            }
-        }
-    }
-    void EnemySpawn(Vector2 position)
-    {
-        enemyObject = ObjectPoolManager.Instance.GetPooledObject("normalenemy");
-        enemyObject.SetActive(true);
-        enemyObject.transform.position = position;
-        enemies.Add(enemyObject);
-        spawnCount++;
-    }
-
-    bool CheckPosition(Vector2 position)
-    {
-        for (int i = (int)insideMin.x; i <= (int)insideMax.x; ++i)
-        {
-            for (int j = (int)insideMin.y; j <= (int)insideMax.y; ++j)
-            {
-                if (position == new Vector2(i, j))
-                {
-                    return false;
-                }
-            }
-        }
-        return true;
-    }
-
-    private void OnDrawGizmos()
-    {
-        Gizmos.DrawLine(insideMin, insideMax);
+        
     }
 }
---
+++ Tyrant/Assets/Scripts/Enemy/TestTarget/PLayerTest.cs
@@ -0,0 +1,50 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PLayerTest : MonoBehaviour,IDamageable,GameObjectsLocator.IGameObjectRegister
+{
+    public string Pname;
+
+    public float time;
+    public void RegisterToLocator()
+    {
+        GameObjectsLocator.Instance.Register<PLayerTest>(this);      
+    }
+
+    public void TakeDamage(float damage)
+    {
+        throw new System.NotImplementedException();
+    }
+
+    public void UnRegisterToLocator()
+    {
+        GameObjectsLocator.Instance.Unregister<PLayerTest>(this);
+        gameObject.SetActive(false);
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        RegisterToLocator();
+        StartCoroutine(deactive());
+    }
+
+    IEnumerator deactive()
+    {
+        yield return new WaitForSeconds(time);
+        UnRegisterToLocator();
+    }
+    // Update is called once per frame
+    //void Update()
+    //{
+    //    if(Input.GetKey(KeyCode.Space ))
+    //    {
+    //        var test = GameObjectsLocator.Instance.Get<PLayerTest>();
+    //        foreach (var item in test)
+    //        {
+    //            Debug.Log(item.Pname);
+    //        }
+    //    }
+    //}
+}

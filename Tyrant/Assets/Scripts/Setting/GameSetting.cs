052085f18c22e0558acd3fb898499fa459eb2d0d
Kairus-KWL <kev.meng.kml@gmail.com>
2021-08-18 22:45:24 -0700

    Updated develop
    - Updated game setting prefab(updated keycode).

--- Tyrant/Assets/Scripts/Setting/GameSetting.cs
+++ Tyrant/Assets/Scripts/Setting/GameSetting.cs
@@ -4,8 +4,33 @@ using UnityEngine;
 
 public class GameSetting : MonoBehaviour
 {
+    private bool gamePause= false;
+
+    public bool GamePause 
+       { 
+        get => gamePause;
+        set {
+                gamePause = value;
+                if (gamePause)
+                {
+                    Time.timeScale =0.0f;
+                }
+                else
+                {
+                    Time.timeScale = 1.0f;
+                }
+             }  
+       }
+
     private void Awake()
     {
         DontDestroyOnLoad(gameObject);
+    }
+    private void Update()
+    {
+        if (InputManager.Instance.GetKeyDown("GamePause"))
+        {
+            GamePause = !GamePause;
+        }
     }
 }

8cd5c85f2553dad54608eeb8e6f9db3db0195237
Kairus-KWL <kev.meng.kml@gmail.com>
2021-09-02 17:39:14 -0700

    Merged branch
    - Merged Develop_ZhenFang branch.
    - Fixed SpawnArea scripts bug. (Missing feature after merged).

--- Tyrant/Assets/Scripts/Enemy/Spawn/SpawnArea.cs
+++ Tyrant/Assets/Scripts/Enemy/Spawn/SpawnArea.cs
@@ -13,15 +13,27 @@ public class SpawnArea : MonoBehaviour, GameObjectsLocator.IGameObjectRegister
 
     [SerializeField]
     private Wave wave;
+
+    [SerializeField]
+    private int itemDropNum;
+
+    [SerializeField]
+    private bool waveDelayTurnOn;
     private int spawnCount;
     private int currentWave;
     private float delayTime;
     private bool spawnClear;
+    private int itemDropCount;
+    private int chooseEnemyDrop;
+    private int totalEnemies;
 
     public int spWidth;
     public int spHeight;
     public int roomNumber;
+    [SerializeField]
+    private SpawnUI spawnUI;
 
+    List<int> dropNumber;
     List<GameObject> enemies;
 
     Collider2D mCollider;
@@ -32,10 +44,16 @@ public class SpawnArea : MonoBehaviour, GameObjectsLocator.IGameObjectRegister
     public float WaveDelay { get => waveDelay; }
     public int CurrentWave { get => currentWave; set => currentWave = value; }
     public int SpawnCount { get => spawnCount; set => spawnCount = value; }
-    public List<GameObject> Enemies { get => enemies; set => enemies = value; }
+    public int ItemDropCount { get => itemDropCount; set => itemDropCount = value; }
+    public int ItemDropNum { get => itemDropNum; set => itemDropNum = value; }
     public float DelayTime { get => delayTime; set => delayTime = value; }
-    public bool SpawnClear { get => spawnClear; set => spawnClear = value; }
+    public List<int> DropNumber { get => dropNumber; set => dropNumber = value; }
+    public List<GameObject> Enemies { get => enemies; set => enemies = value; }
     public Wave Wave { get => wave; }
+    public bool SpawnClear { get => spawnClear; set => spawnClear = value; }
+    public bool WaveDelayTurnOn { get => waveDelayTurnOn; }
+    public int ChooseEnemyDrop { get => chooseEnemyDrop; set => chooseEnemyDrop = value; }
+    public int TotalEnemies { get => totalEnemies; set => totalEnemies = value; }
 
 
     //  public string[] enemyType = { "normalenemy", "rangeEnemy" };
@@ -49,6 +67,7 @@ public class SpawnArea : MonoBehaviour, GameObjectsLocator.IGameObjectRegister
         Area();
         Enemies = new List<GameObject>();
         Portal.GetComponent<Portal>().index = roomNumber;
+        DropNumber = new List<int>();
         RegisterToLocator();
     }
     private void Update()

62240d86016f867869c0fa545ff8dcf1d90cd10c
DESKTOP-H2K2OPM\pochun <doghot0625@gmail.com>
2021-09-05 21:37:48 -0700

    PochunChen
    add boss
    add boss health UI
    add boss  collider

--- Tyrant/Assets/Scripts/Enemy/Boss/PSC.cs
+++ Tyrant/Assets/Scripts/Enemy/Boss/PSC.cs
@@ -1,20 +1,66 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
-public class PSC : MonoBehaviour
+public class PSC : MonoBehaviour, GameObjectsLocator.IGameObjectRegister, IDamageable
 {
+    public Slider healthSilder;
     [SerializeField]
     private EnemyState enemyState;
 
     public EnemyState EnemyState { get => enemyState; set => enemyState = value; }
 
     private float health = 100.0f;
+
+    private bool isDead = false;
     public bool IsDied
     {
-        get
+        get => isDead;
+        set
         {
-            return health <= 0f;
+            if (isDead)
+            {
+                gameObject.SetActive(false);
+                UnRegisterToLocator();
+            }
+            isDead = value;
+        }
+    }
+
+    private void Start()
+    {
+        healthSilder.maxValue = EnemyState.MaxHealth;
+        health = EnemyState.MaxHealth;
+    }
+    private void Update()
+    {
+        healthSilder.value = health;
+    }
+    public void RegisterToLocator()
+    {
+        GameObjectsLocator.Instance.Register<PSC>(this);
+    }
+
+    public void UnRegisterToLocator()
+    {
+        GameObjectsLocator.Instance.Unregister<PSC>(this);
+    }
+
+    public void TakeDamage(float damage)
+    {
+        health -= damage;
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.tag == "Player" || collision.gameObject.tag == "Tower" || collision.gameObject.tag == "Base")
+        {
+            IDamageable Targets = collision.gameObject.GetComponent<IDamageable>();
+            if (Targets == null)
+            {
+                Targets = collision.gameObject.GetComponentInChildren<IDamageable>();
+            }
+            Targets.TakeDamage(EnemyState.MaxDamage);
+            Debug.Log("attack");
         }
     }
 }

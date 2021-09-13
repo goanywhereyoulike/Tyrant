6c7fe756a189cb87c11b939643302d9b9d147a3a
william chen <53885588+YourFather69420@users.noreply.github.com>
2021-09-13 16:01:27 -0700

    zhiming chen
    -added skip dialogue
    -fixed dialogue triggering repeatedly

--- Tyrant/Assets/Scripts/UI/DialogUI/DialogueActivate.cs
+++ Tyrant/Assets/Scripts/UI/DialogUI/DialogueActivate.cs
@@ -6,28 +6,51 @@ public class DialogueActivate : MonoBehaviour,IInteractable
 {
     [SerializeField] private DialogueObject dialogueObject;
     public Image tutorialButton;
-
+    private bool isTriggered = false;
+    private bool canTrigger;
+    private Player player;
+    private void Start()
+    {
+        isTriggered = false;
+    }
+    private void Update()
+    {
+        if (canTrigger && InputManager.Instance.GetKeyDown("Interact"))
+        { 
+            Time.timeScale = 0;
+            Interact(player);
+            isTriggered = true;
+            canTrigger = false;
+        }
+    }
     private void OnTriggerEnter2D(Collider2D collision)
     {
-        if (collision.CompareTag("Player") && collision.TryGetComponent(out Player player))
+        if (!isTriggered)
         {
-            player.interactable = this;
-            tutorialButton.enabled = true;
+            if (collision.CompareTag("Player") && collision.TryGetComponent(out Player players))
+            {
+                player = players;
+                tutorialButton.enabled = true;
+                canTrigger = true;
+            }
         }
     }
     private void OnTriggerExit2D(Collider2D collision)
     {
-        if (collision.CompareTag("Player") && collision.TryGetComponent(out Player player))
+        if (collision.CompareTag("Player") && collision.TryGetComponent(out Player players))
         {
-            if (player.interactable is DialogueActivate dialogueActive && dialogueActive == this)
-            {
-                player.interactable = null;
-                tutorialButton.enabled = false;
-            }
+            //player = null;
+            tutorialButton.enabled = false;
+            canTrigger = false;
         }
     }
+    
     public void Interact(Player player)
     {
+        isTriggered = true;
         player.DialogueUI.ShowDialogue(dialogueObject);
     }
+   
+     
 }
+

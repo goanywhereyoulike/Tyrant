89695db8de7d4e641c52f77878f1586cdf1d183e
震 方 <a1521442784@live.com>
2021-11-14 23:30:17 -0800

    [MIniMap] -Added Button M to control the appearance of minimap

--- Tyrant/Assets/Scripts/MiniMap/MiniMap.cs
+++ Tyrant/Assets/Scripts/MiniMap/MiniMap.cs
@@ -5,10 +5,23 @@ using UnityEngine;
 public class MiniMap : MonoBehaviour
 {
     private Transform player;
+    [SerializeField]
+    GameObject MiniMapUI;
+
+    bool IsOpen = true;
     // Start is called before the first frame update
     private void Start()
     {
         player = FindObjectOfType<Player>().transform;
+    }
+
+    private void Update()
+    {
+        if (InputManager.Instance.GetKeyDown("MiniMap"))
+        {
+            MiniMapUI.SetActive(IsOpen);
+            IsOpen = !IsOpen;
+        }
     }
     private void LateUpdate()
     {

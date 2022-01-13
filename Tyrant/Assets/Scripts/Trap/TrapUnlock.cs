0f5898708dce3fda6794a3434bba8bb33fe63671
震 方 <a1521442784@live.com>
2022-01-13 11:57:30 -0800

    [DoTween] -Added DoTween plugin -Added trap animation when interacting

--- Tyrant/Assets/Scripts/Trap/TrapUnlock.cs
+++ Tyrant/Assets/Scripts/Trap/TrapUnlock.cs
@@ -1,9 +1,11 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using DG.Tweening;
 
 public class TrapUnlock : MonoBehaviour
 {
+    public Vector2 AnimatePos;
     public bool isInfinite = false;
     public int index = -1;
     private void OnTriggerEnter2D(Collider2D collision)
@@ -12,17 +14,18 @@ public class TrapUnlock : MonoBehaviour
 
         if (player)
         {
-           // ManagerSwitch mangerSwitch = FindObjectOfType<ManagerSwitch>();
+            // ManagerSwitch mangerSwitch = FindObjectOfType<ManagerSwitch>();
 
             //TrapManager TrapMngr = FindObjectOfType<TrapManager>();
 
             TrapManager.Instance.AddTrap(index);
+            gameObject.transform.DOShakeScale(0.8f).OnComplete(()=> { transform.DORewind(); });
 
             if (!isInfinite)
             {
                 gameObject.SetActive(false);
             }
-           // Destroy(gameObject);
+            // Destroy(gameObject);
 
         }
     }
